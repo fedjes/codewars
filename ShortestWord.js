1a3d84563447341fe38c9cde4bdae91028deacc3
fedjes <salvados78@mail.ru>
2024-08-19 15:40:52 +0300

    done

--- ShortestWord.js
+++ ShortestWord.js
@@ -2,3 +2,7 @@
 
 // String will never be empty and you do not need to account for different data types.
 
+function findShort(s){
+    let arr = s.split(' ')
+    return arr.reduce((acc,cur) => (c.length < a.length ? cur : acc), arr[0]).length
+  }
